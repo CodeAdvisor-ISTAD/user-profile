a2dd560437347b803800c779ad45565e9e24a222
lyzhia <eunglyzhia@gmail.com>
2024-12-23 15:15:35 +0700

    add config server into project

---
+++ src/main/java/istad/codeadvisor/userservice/config/KafkaConfig.java
@@ -0,0 +1,9 @@
+package istad.codeadvisor.userservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@EnableKafka
+@Configuration
+public class KafkaConfig {
+}
